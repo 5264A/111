10fd7f86aee19d0c57b3223b51bb44c60d0faf77
Aman Bhavsar <96308303+5264A@users.noreply.github.com>
2024-07-05 10:19:26 +0530

    Update Footer.jsx

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -8,16 +8,16 @@ const Footer = () => {
         <Stack direction={["column","row"]}>
             <VStack alignItems={["center","flex-start"]} width={"full"}>
                 <Heading children="All Rights Reserved" color='white' />
-                <Heading children="@Course Bundler" fontFamily={"body"} size={"sm"} color='yellow.400' />
+                <Heading children="@Aman" fontFamily={"body"} size={"sm"} color='yellow.400' />
             </VStack>
             <HStack color={"white"} fontSize={"40"} justifyContent={"center"} spacing={["2","10"]}>
-                <a href="https://youtube.com" target={'_blank'}>
+                <a href="https://www.instagram.com/aman____9001/" target={'_blank'}>
                     <TiSocialYoutube/>
                 </a> 
-                <a href="https://facebook.com" target={'_blank'}>
+                <a href="https://www.instagram.com/aman____9001/" target={'_blank'}>
                     <TiSocialFacebook/>
                 </a> 
-                <a href="https://instagram.com" target={'_blank'}>
+                <a href="https://www.instagram.com/aman____9001/" target={'_blank'}>
                     <TiSocialInstagram/>
                 </a>
             </HStack>
